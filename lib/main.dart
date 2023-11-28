37fe8d231a492972255a24918dd4a54686178d29
benvenuto <benve1997@libero.it>
2023-11-28 23:34:10 +0100

    mia versione della terza app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:third_app/gradient_container.dart';
-void main()
-{
-  //const List<Color> listcolour=[ Colors.deepPurple, Colors.purpleAccent];
-  runApp( MaterialApp(
-    home:Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body:Column(
-          children: [
-            Image.asset('assets/images/quiz-logo.png'),
-            Text('ok'),
-          ],
-        )// GradientContainer(listcolour)
 
-    )
-  ));
+void main() {
+  const bkColor = Colors.deepPurple;
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: bkColor,
+        body: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 200),
+            const SizedBox(height: 50),
+            const Text('Learn flutter the fun Way',
+                style: TextStyle(color: Colors.white, fontSize: 20)),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Add your button click logic here
+              },
+              child: const Text(
+                'Start quiz',
+                style: TextStyle(
+                  color: Colors
+                      .white, // Set text color to match the background color
+                ),
+              ),
+            ),
+          ],
+        )
+            ),
+      ),
+    ),
+  );
 }
