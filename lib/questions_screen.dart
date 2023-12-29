6341254f37f637bb51226f98313b178fef370304
benvenuto <benve1997@libero.it>
2023-12-29 15:13:05 +0100

    lezione 73

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
-class QuestionScreen extends StatefulWidget{
+import 'package:third_app/answer_button.dart';
+import 'package:third_app/models/quiz_question.dart';
+
+import 'data/questions.dart';
+
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
+
   @override
   State<QuestionScreen> createState() {
-  return _QuestionScreenState();
+    return _QuestionScreenState();
   }
-
 }
-class _QuestionScreenState extends State<QuestionScreen>{
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex=0;
+
   @override
-  Widget build(context){
-    return Text('question_Screen');
+  Widget build(context) {
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswer().map((answer){
+              return AnswerButton(answerText: answer,onTap: (){});
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
