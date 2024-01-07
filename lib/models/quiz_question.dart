36de39ebab04371031ccdf18570862bea558b545
benvenuto <benve1997@libero.it>
2024-01-07 17:27:21 +0100

    in verifica

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,9 +1,11 @@
-class QuizQuestion{
-  const QuizQuestion(this.text,this.answer);
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answer);
+
   final String text;
   final List<String> answer;
-  List<String> getShuffledAnswer(){
-    final shuffledList=List.of(answer);
+
+  List<String> getShuffledAnswer() {
+    final shuffledList = List.of(answer);
     shuffledList.shuffle();
     return shuffledList;
   }
